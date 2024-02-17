be324ea7ae123f98fcd5d81945630f1a453769c7
Muhammad Ibrahim Ahsan <meeibrahim39@gmail.com>
2024-02-17 19:02:34 +0500

    Added skills

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import Navbar from "./components/Navbar";
-import DummySection from "./components/Dummy";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -1,34 +1,34 @@
 export const projects = [
   {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
-  },
-  {
-    title: "React Tracks",
-    subtitle: "React and Python",
+    title: "C.A.R.E",
+    subtitle: "Selenium Grid inside K8 Cluster",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-2.gif",
     link: "https://reedbarger.com",
   },
   {
-    title: "DevChat",
-    subtitle: "React and Firebase",
+    title: "Automation with Intelligent State Management",
+    subtitle: "Java and Selenium",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-3.gif",
     link: "https://jsbootcamp.com",
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
+    title: "API Framework for Automation",
+    subtitle: "Java with Apache Http5",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-4.gif",
     link: "https://pythonbootcamp.com",
+  },
+  {
+    title: "This Website!",
+    subtitle: "React with Tailwind",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
   },
 ];
